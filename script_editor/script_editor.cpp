a8ea8a81e0f48c109cdfd064975e0886e64b9773
Lukas Cone <lukas.kuzel.ch@gmail.com>
2025-06-28 19:29:29 +0200

    update script editor

--- script_editor/script_editor.cpp
+++ script_editor/script_editor.cpp
@@ -3,8 +3,7 @@
 #include <GLFW/glfw3.h>
 
 #include "font_awesome4/definitions.h"
-#include "imgui_impl_glfw.h"
-#include "imgui_impl_opengl3.h"
+#include "imgui.h"
 
 #include "script_editor.hpp"
 
@@ -12,12 +11,12 @@
 #include "spike/io/fileinfo.hpp"
 #include "spike/master_printer.hpp"
 #include "spike/util/unicode.hpp"
+#include "widgets.hpp"
+#include "window.hpp"
 
-#include <condition_variable>
+#include <filesystem>
 #include <fstream>
-#include <list>
 #include <sstream>
-#include <thread>
 
 #include "squirrel.h"
 
@@ -27,63 +26,6 @@
 #include "sqstdmath.h"
 #include "sqstdstring.h"
 #include "sqstdsystem.h"
-
-void WarmColors() {
-  ImVec4 *colors = ImGui::GetStyle().Colors;
-  colors[ImGuiCol_Text] = ImVec4(1.f, 1.f, 0.95f, 1.f);
-  colors[ImGuiCol_TextDisabled] = ImVec4(0.50f, 0.50f, 0.50f, 1.00f);
-  colors[ImGuiCol_WindowBg] = ImVec4(0.06f, 0.06f, 0.06f, 0.94f);
-  colors[ImGuiCol_ChildBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-  colors[ImGuiCol_PopupBg] = ImVec4(0.08f, 0.08f, 0.08f, 0.94f);
-  colors[ImGuiCol_Border] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
-  colors[ImGuiCol_BorderShadow] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-  colors[ImGuiCol_FrameBg] = ImVec4(0.29f, 0.29f, 0.29f, 0.54f);
-  colors[ImGuiCol_FrameBgHovered] = ImVec4(0.53f, 0.53f, 0.53f, 0.40f);
-  colors[ImGuiCol_FrameBgActive] = ImVec4(0.64f, 0.64f, 0.64f, 0.67f);
-  colors[ImGuiCol_TitleBg] = ImVec4(0.04f, 0.04f, 0.04f, 1.00f);
-  colors[ImGuiCol_TitleBgActive] = ImVec4(0.17f, 0.15f, 0.12f, 1.00f);
-  colors[ImGuiCol_TitleBgCollapsed] = ImVec4(0.00f, 0.00f, 0.00f, 0.51f);
-  colors[ImGuiCol_MenuBarBg] = ImVec4(0.14f, 0.14f, 0.14f, 1.00f);
-  colors[ImGuiCol_ScrollbarBg] = ImVec4(0.02f, 0.02f, 0.02f, 0.53f);
-  colors[ImGuiCol_ScrollbarGrab] = ImVec4(0.31f, 0.31f, 0.31f, 1.00f);
-  colors[ImGuiCol_ScrollbarGrabHovered] = ImVec4(0.41f, 0.41f, 0.41f, 1.00f);
-  colors[ImGuiCol_ScrollbarGrabActive] = ImVec4(0.51f, 0.51f, 0.51f, 1.00f);
-  colors[ImGuiCol_CheckMark] = ImVec4(0.00f, 1.00f, 0.02f, 1.00f);
-  colors[ImGuiCol_SliderGrab] = ImVec4(0.49f, 0.58f, 0.69f, 1.00f);
-  colors[ImGuiCol_SliderGrabActive] = ImVec4(0.62f, 0.67f, 0.73f, 1.00f);
-  colors[ImGuiCol_Button] = ImVec4(0.70f, 0.70f, 0.70f, 0.40f);
-  colors[ImGuiCol_ButtonHovered] = ImVec4(0.19f, 0.29f, 0.40f, 1.00f);
-  colors[ImGuiCol_ButtonActive] = ImVec4(0.06f, 0.53f, 0.98f, 1.00f);
-  colors[ImGuiCol_Header] = ImVec4(1.00f, 0.99f, 0.92f, 0.31f);
-  colors[ImGuiCol_HeaderHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.80f);
-  colors[ImGuiCol_HeaderActive] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  colors[ImGuiCol_Separator] = ImVec4(0.43f, 0.43f, 0.50f, 0.50f);
-  colors[ImGuiCol_SeparatorHovered] = ImVec4(0.10f, 0.40f, 0.75f, 0.78f);
-  colors[ImGuiCol_SeparatorActive] = ImVec4(0.10f, 0.40f, 0.75f, 1.00f);
-  colors[ImGuiCol_ResizeGrip] = ImVec4(0.26f, 0.59f, 0.98f, 0.20f);
-  colors[ImGuiCol_ResizeGripHovered] = ImVec4(0.26f, 0.59f, 0.98f, 0.67f);
-  colors[ImGuiCol_ResizeGripActive] = ImVec4(0.26f, 0.59f, 0.98f, 0.95f);
-  colors[ImGuiCol_Tab] = ImVec4(0.43f, 0.40f, 0.31f, 0.86f);
-  colors[ImGuiCol_TabHovered] = ImVec4(0.33f, 0.33f, 0.18f, 0.80f);
-  colors[ImGuiCol_TabActive] = ImVec4(0.66f, 0.63f, 0.19f, 1.00f);
-  colors[ImGuiCol_TabUnfocused] = ImVec4(0.07f, 0.10f, 0.15f, 0.97f);
-  colors[ImGuiCol_TabUnfocusedActive] = ImVec4(0.33f, 0.36f, 0.12f, 1.00f);
-  colors[ImGuiCol_PlotLines] = ImVec4(0.61f, 0.61f, 0.61f, 1.00f);
-  colors[ImGuiCol_PlotLinesHovered] = ImVec4(1.00f, 0.43f, 0.35f, 1.00f);
-  colors[ImGuiCol_PlotHistogram] = ImVec4(0.90f, 0.70f, 0.00f, 1.00f);
-  colors[ImGuiCol_PlotHistogramHovered] = ImVec4(1.00f, 0.60f, 0.00f, 1.00f);
-  colors[ImGuiCol_TableHeaderBg] = ImVec4(0.19f, 0.19f, 0.20f, 1.00f);
-  colors[ImGuiCol_TableBorderStrong] = ImVec4(0.31f, 0.31f, 0.35f, 1.00f);
-  colors[ImGuiCol_TableBorderLight] = ImVec4(0.23f, 0.23f, 0.25f, 1.00f);
-  colors[ImGuiCol_TableRowBg] = ImVec4(0.00f, 0.00f, 0.00f, 0.00f);
-  colors[ImGuiCol_TableRowBgAlt] = ImVec4(1.00f, 1.00f, 1.00f, 0.06f);
-  colors[ImGuiCol_TextSelectedBg] = ImVec4(0.26f, 0.59f, 0.98f, 0.35f);
-  colors[ImGuiCol_DragDropTarget] = ImVec4(1.00f, 1.00f, 0.00f, 0.90f);
-  colors[ImGuiCol_NavHighlight] = ImVec4(0.26f, 0.59f, 0.98f, 1.00f);
-  colors[ImGuiCol_NavWindowingHighlight] = ImVec4(1.00f, 1.00f, 1.00f, 0.70f);
-  colors[ImGuiCol_NavWindowingDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.20f);
-  colors[ImGuiCol_ModalWindowDimBg] = ImVec4(0.80f, 0.80f, 0.80f, 0.35f);
-}
 
 std::vector<std::string> output;
 
@@ -124,9 +66,13 @@ template <size_t N> void sq_pushstring(HSQUIRRELVM v, const char (&str)[N]) {
 }
 
 #define DBG_TOP(...)                                                           \
-  { printf("%i: %lli " #__VA_ARGS__, __LINE__, sq_gettop(v)); }                \
+  {                                                                            \
+    printf("%i: %lli " #__VA_ARGS__, __LINE__, sq_gettop(v));                  \
+  }                                                                            \
   __VA_ARGS__;                                                                 \
-  { printf(" %lli\n", sq_gettop(v)); }
+  {                                                                            \
+    printf(" %lli\n", sq_gettop(v));                                           \
+  }
 
 void sq_compile(HSQUIRRELVM v, std::stringstream text, const char *fileName) {
   auto readChar = [](SQUserPointer iobuf) -> SQInteger {
@@ -335,138 +281,8 @@ static SQInteger _sqstd_aux_printerror(HSQUIRRELVM v) {
   return 0;
 }
 
-#include <filesystem>
-
-struct FolderTree {
-  std::filesystem::path fullPath;
-  std::string folderName;
-  std::vector<std::unique_ptr<FolderTree>> children{};
-  bool scanned = false;
-  bool isFolder = true;
-};
-
-void ScanTree(FolderTree &tree) {
-  namespace fs = std::filesystem;
-  fs::directory_iterator dirIt(tree.fullPath);
-  tree.scanned = true;
-
-  for (auto &entry : dirIt) {
-    if (!entry.is_directory() && entry.path().extension() != ".nut") {
-      continue;
-    }
-    tree.children.emplace_back(std::make_unique<FolderTree>(FolderTree{
-        .fullPath = entry,
-        .folderName = entry.path().filename(),
-        .isFolder = entry.is_directory(),
-    }));
-  }
-}
-
-void DrawFolderTree(FolderTree &tree, uint32 level, uint32 index,
-                    std::filesystem::path &selectedPath) {
-  uint64 ptrId = index | (uint64(level) >> 32);
-  ImGuiTreeNodeFlags nodeFlags = tree.isFolder ? 0 : ImGuiTreeNodeFlags_Leaf;
-  const bool opened = ImGui::TreeNodeEx(
-      reinterpret_cast<void *>(ptrId), nodeFlags,
-      tree.isFolder ? (ICON_FA_FOLDER " %s") : (ICON_FA_FILE " %s"),
-      tree.folderName.c_str());
-
-  if (!tree.isFolder && ImGui::IsMouseDoubleClicked(ImGuiMouseButton_Left) &&
-      ImGui::IsItemClicked(ImGuiMouseButton_Left)) {
-    selectedPath = tree.fullPath;
-  }
-
-  if (!tree.isFolder) {
-    ImGui::TreePop();
-    return;
-  }
-
-  if (opened && !tree.scanned && tree.isFolder) {
-    ScanTree(tree);
-  }
-
-  if (opened) {
-    uint32 childIndex = 0;
-
-    for (auto &f : tree.children) {
-      DrawFolderTree(*f, level + 1, childIndex++, selectedPath);
-    }
-
-    ImGui::TreePop();
-  } else if (tree.scanned) {
-    tree.scanned = false;
-    tree.children.clear();
-  }
-}
-
 void sqigui_register_ImVec2(HSQUIRRELVM v);
 void sqigui_register_ImDrawList(HSQUIRRELVM v);
-
-struct Window {
-  GLFWwindow *window;
-  ImGuiContext *context;
-};
-
-int InitWindow(Window &wnd, int width, int height, const char *name,
-               Window *share = nullptr) {
-  wnd.window = glfwCreateWindow(width, height, name, nullptr,
-                                share ? share->window : nullptr);
-
-  if (!wnd.window) {
-    glfwTerminate();
-    return 1;
-  }
-
-  static bool GLEW_INITED = false;
-
-  if (!GLEW_INITED) {
-    GLEW_INITED = true;
-    glfwMakeContextCurrent(wnd.window);
-    GLenum err = glewInit();
-
-    if (GLEW_OK != err) {
-      glfwTerminate();
-      return 2;
-    }
-  }
-
-  wnd.context = ImGui::CreateContext();
-  ImGui::SetCurrentContext(wnd.context);
-  ImGui_ImplGlfw_InitForOpenGL(wnd.window, true);
-  ImGui_ImplGlfw_SetCallbacksChainForAllWindows(false);
-  ImGui_ImplOpenGL3_Init();
-
-  return 0;
-}
-
-void SwitchWindow(Window &wnd) {
-  glfwMakeContextCurrent(wnd.window);
-  ImGui::SetCurrentContext(wnd.context);
-}
-
-void WindowBegin(Window &wnd) {
-  SwitchWindow(wnd);
-  glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-  glClear(GL_COLOR_BUFFER_BIT);
-
-  ImGui_ImplOpenGL3_NewFrame();
-  ImGui_ImplGlfw_NewFrame();
-  ImGui::NewFrame();
-}
-
-void WindowEnd(Window &wnd) {
-  ImGui::Render();
-  ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-
-  glfwSwapBuffers(wnd.window);
-}
-
-void DestroyWindow(Window &wnd) {
-  SwitchWindow(wnd);
-  ImGui_ImplOpenGL3_Shutdown();
-  ImGui_ImplGlfw_Shutdown();
-  glfwDestroyWindow(wnd.window);
-}
 
 int main(int, char *[]) {
   es::print::AddPrinterFunction(es::Print);
@@ -491,15 +307,6 @@ int main(int, char *[]) {
   if (int status = InitWindow(previewWnd, 1280, 720, "Preview"); status) {
     return status;
   }
-
-  // glfwWindowHint(GLFW_VISIBLE, GLFW_FALSE);
-  // GLFWwindow *previewWnd =
-  //    glfwCreateWindow(1280, 720, "Preview", nullptr, window);
-
-  // glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
-  // glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 5);
-  // glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-  // glfwWindowHint(GLFW_CONTEXT_RELEASE_BEHAVIOR, GLFW_RELEASE_BEHAVIOR_FLUSH);
 
   SwitchWindow(mainWnd);
   WarmColors();
@@ -535,7 +342,8 @@ int main(int, char *[]) {
   sq_newclosure(v, _sqstd_aux_printerror, 0);
   sq_seterrorhandler(v);
 
-  FolderTree rootTree{.fullPath = "scripts", .folderName = "scripts"};
+  FolderTree rootTree{
+      .fullPath = "scripts", .folderName = "scripts", .extensions = {".nut"}};
 
   while (!glfwWindowShouldClose(mainWnd.window)) {
     WindowBegin(mainWnd);
@@ -647,7 +455,7 @@ int main(int, char *[]) {
 
     ImGui::End();
     WindowEnd(previewWnd);
-    //glfwPollEvents();
+    // glfwPollEvents();
   }
 
   DestroyWindow(mainWnd);
